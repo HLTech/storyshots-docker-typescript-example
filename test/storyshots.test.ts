f72e664ece5dd31b696ed39e3f0e5eb5419aba68
lukasz.fiszer <lukasz.fiszer@hltech.com>
2019-05-27 13:43:23 +0200

    Fix URL to storybook build on CI

--- test/storyshots.test.ts
+++ test/storyshots.test.ts
@@ -1,5 +1,4 @@
 import * as path from 'path';
-import {pathToFileURL} from "url";
 import initStoryshots from '@storybook/addon-storyshots';
 import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer';
 import {connect} from "puppeteer";
@@ -20,9 +19,9 @@ const CHROMIUM_URL = 'http://127.0.0.1:19222';
 const STORYBOOK_DEV_URL = 'http://host.docker.internal:9001';
 
 /**
- * Define URL to static build of Storybook
+ * URL to static build of Storybook, accessible from inside docker container
  */
-const STORYBOOK_BUILD_URL = pathToFileURL(path.join(__dirname, '../dist'));
+const STORYBOOK_BUILD_URL = "file:///storybook_build/";
 
 /**
  * Function returning config object that will be passed to jest-image-snapshot
